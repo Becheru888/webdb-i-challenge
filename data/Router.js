740dbbb30bc3e07dc5997fe4547d35aeb0956b89
Becheru888 <becheru93@gmail.com>
2019-07-15 16:16:04 +0100

    MVP DONE & STREACH

--- data/Router.js
+++ data/Router.js
@@ -1,9 +1,6 @@
 const express =  require('express')
-
-const Accounts = require('../data/seeds/accounts-model')
+const Accounts = require('./seeds/accounts-model')
 const router = express.Router()
-
-
 
 
 
@@ -17,5 +14,51 @@ router.get('/', (req,res) => {
     })
 })
 
+router.get("/:id", async (req, res) => {
+    try {
+      const account = await Accounts.getAccountById(req.params.id);
+      res.status(200).json(account);
+    } catch (error) {
+      res.status(500).json({ message: `Could not retrieve account with ID ${req.params.id}` });
+    }
+  });
+
+router.post('/', async (req, res) =>{
+    try{
+        const newAccount = await Accounts.createAccount(req.body)
+        res.status(201).json(newAccount)
+    }catch(error){
+        res.status(500).json('Account faild to create')
+    }
+
+})
+
+router.put("/:id", async (req, res) => {
+    try {
+      const { name, budget } = req.body;
+      if (!req.body.name || !req.body.budget) {
+        res.status(500).json({ message: "Name and budget requierd" });
+      } else {
+        const updatedAccount = await Accounts.getAccountById(req.params.id);
+        const target = await Accounts.updateAccountById(updatedAccount, {name,budget});
+        res.status(200).json(target);
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: `Could not update account with ID ${req.params.id}.`
+      });
+    }
+  });
+
+  router.delete("/:id", async (req, res) => {
+    try {
+       await Accounts.deleteAccountById(req.params.id);
+      res.status(200).json({ message: `Account ${req.params.id} has been deleted` });
+    } catch (error) {
+      res.status(500).json({
+        message: `Could not delete account ${req.params.id}.`
+      });
+    }
+  });
 
 module.exports = router;
--- data/seeds/accounts-model.js
+++ data/seeds/accounts-model.js
@@ -1,13 +1,40 @@
-const db = require('../dbConfig')
+const db = require('../../data/dbConfig')
 
 module.exports = {
     find,
+    createAccount,
+    getAccountById,
+    updateAccountById,
+    deleteAccountById,
+
 }
+
+
 
 
 function find(){
     return db('accounts');
 }
+
+function createAccount({name, budget}){
+    return db("accounts").insert({name, budget})
+}
+
+function getAccountById(id) {
+    return db('accounts').where({ id });
+  }
+
+  function updateAccountById(id, { name, budget }) {
+    return db("accounts")
+      .where({ id })
+      .update({ name, budget });
+  }
+ 
+  function deleteAccountById(id) {
+    return db("accounts")
+      .where({ id })
+      .del();
+  }
 
 
 
